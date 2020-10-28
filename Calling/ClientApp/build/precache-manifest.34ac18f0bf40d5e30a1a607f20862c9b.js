335ad263afbf77930e538c0c3f0f8e86fff6bb62
Takunori Min <taminami@microsoft.com>
2020-10-28 15:14:19 +0900

    first commit

---
+++ Calling/ClientApp/build/precache-manifest.34ac18f0bf40d5e30a1a607f20862c9b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "184e6a0d4e18cb5eabd9a57605560805",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78a8aedf65093cf41e84",
+    "url": "/static/css/main.74a01892.chunk.css"
+  },
+  {
+    "revision": "b9e7c664acd8cd1dede1",
+    "url": "/static/js/2.b6cea037.chunk.js"
+  },
+  {
+    "revision": "657eb59561c872f5e7967385311b1910",
+    "url": "/static/js/2.b6cea037.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78a8aedf65093cf41e84",
+    "url": "/static/js/main.622625d6.chunk.js"
+  },
+  {
+    "revision": "3050cbde0abbb25c83ef",
+    "url": "/static/js/runtime-main.a1ff89ce.js"
+  },
+  {
+    "revision": "4acf60cc1bfc4f06b5ae6f878956b571",
+    "url": "/static/media/hero.4acf60cc.svg"
+  },
+  {
+    "revision": "194569552cbf294a7f3434011cb57818",
+    "url": "/static/media/staticmedia.19456955.svg"
+  }
+]);
